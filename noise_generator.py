79d0ae9c9e958d8ea0a92892b26c33e8b2d6c0eb
serabiasin <ahmadalfiazdzikri@gmail.com>
2018-12-22 11:06:22 +0700

    Initial

---
+++ noise_generator.py
@@ -0,0 +1,37 @@
+#! /usr/env/bin python3
+
+import argparse
+import os
+import sys
+import glob
+import pickle
+import numpy as np
+
+
+"""
+generateNoise
+deskripsi :
+membuat noise dari satu buah file
+
+parameter
+file : file yang akan di augmentasi data nya
+
+
+"""
+# Implemented Soon
+def generateNoise(file):
+    pass
+
+
+direktori_root=""
+
+parser = argparse.ArgumentParser(description='Tools untuk membuat Noise pada file wav')
+parser.add_argument('--dir', help="Target Direktori ")
+
+args = parser.parse_args()
+Direktori=args.dir
+
+sub_dir=[]
+for file in os.listdir(Direktori):
+    if os.path.isdir(file):
+        sub_dir.append(file)
